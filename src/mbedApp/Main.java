65e47b02ba32af19cbab459851e1e7b1f81014a1
Will Furnell <mail@willfurnell.com>
2017-02-12 14:03:31 +0000

    Make Main class more BlueJ friendly

--- src/mbedApp/Main.java
+++ src/mbedApp/Main.java
@@ -1,11 +1,15 @@
 package mbedApp;
 
-
 import mbedApp.mbed.HomeAutomator;
 
 public class Main {
+    private static Main main;
+    
+    public Main() {
+        HomeAutomator ha = new HomeAutomator();
+    }
 
     public static void main(String[] args) {
-        HomeAutomator test = new HomeAutomator();
+        main = new Main();
     }
 }
