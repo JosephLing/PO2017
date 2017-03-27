a626649304d49b22e3a7d9261071a5eeb7a4cc5b
joe ling <josephling11@gmail.com>
2017-03-27 17:25:46 +0100

    worked on getting the ical info to render in a Jtable issue with rerendering, will leave and move on to pushing to mqtt

--- src/mbedApp/gui/events/EventFrame.java
+++ src/mbedApp/gui/events/EventFrame.java
@@ -1,5 +1,10 @@
 package mbedApp.gui.events;
 import javax.swing.*;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
@@ -7,6 +12,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -19,29 +25,75 @@ public class EventFrame extends JFrame {
 
     private JTextField urlTextField;
     private JLabel response;
+    private JTable repsonseTable;
+    private JScrollPane scrollPane;
 
     public EventFrame() throws HeadlessException {
-        System.out.println(createUrlConnection("a"));
-        System.out.println(wifiTest());
+//        System.out.println(createUrlConnection("a"));
+//        System.out.println(wifiTest());
+//
+//        System.out.println(createUrlConnection("www.kent.ac.uk/timetabling/ical/127379.ics"));
+//        System.out.println(createUrlConnection("webcal://www.kent.ac.uk/timetabling/ical/127379.ics"));
 
-        System.out.println(createUrlConnection("www.kent.ac.uk/timetabling/ical/127379.ics"));
-        System.out.println(createUrlConnection("webcal://www.kent.ac.uk/timetabling/ical/127379.ics"));
-
-        System.out.println("-----");
 //        getFile("https://calendar.google.com/calendar/ical/josephling11%40gmail.com/private-34ea00d303b31413b8d556dfde44763c/basic.ics");
 //        getFile("www.kent.ac.uk/timetabling/ical/127379.ics");
+        this.setTitle("Event sub");
+        // row size, col 4
+
+
+
+        createUrlInput();
+
+        scrollPane = new JScrollPane();
+        this.add(scrollPane, BorderLayout.CENTER);
+        setSize(500,500);
+        this.setVisible(true);
+
         ArrayList<String> a = getFile("https://p60-calendars.icloud.com/published/2/KQpieZ-gmqxhEDixV83NnwiNFshbA0Kv7YrWnWT6RtG4vEt0ZavpPs_Yx2xnoQA8BOLACjFBfqzO4TiaLQUM1BADNcvK_J3GwAHbUY8btOA");
-        createCalender(a);
 
-
-
-
-        //        this.setTitle("Event sub");
-//
-//        createUrlInput();
-//        setSize(500,500);
-//        this.setVisible(true);
+        createTable(a);
     }
+
+    private void createTable(ArrayList<String> a){
+        if (repsonseTable != null){
+            scrollPane.remove(repsonseTable);
+        }
+        Object[][] data = new Object[a.size()][4];
+        ArrayList<IcalEvent> b = createCalender(a);
+        for (int i = 0; i < b.size(); i++) {
+            data[i] = b.get(i).toObjectArray();
+        }
+        repsonseTable = new JTable(data, new String[]{"summary", "timezone", "date", "start"});
+
+        TableModel dtm = new AbstractTableModel() {
+            @Override
+            public int getRowCount() {
+                return b.size();
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 4;
+            }
+
+            @Override
+            public Object getValueAt(int i, int i1) {
+                return  data[i][i1];
+            }
+        };
+        scrollPane.updateUI();
+        repsonseTable.setModel(dtm);
+        scrollPane.add(repsonseTable);
+        scrollPane.validate();
+        scrollPane.repaint();
+        validate();
+        repaint();
+
+        // http://stackoverflow.com/questions/9706682/swing-jscrollpane-doesnt-refresh-after-changes-in-the-structure-of-a-jtable
+        repsonseTable.setFillsViewportHeight(true);
+
+    }
+
 
     private void createUrlInput(){
 
@@ -49,24 +101,36 @@ public class EventFrame extends JFrame {
         urlTextField.setSize(150,20);
         urlTextField.addActionListener((ActionEvent e)->{
             onTextInput();});
-        this.add(urlTextField, BorderLayout.CENTER);
+        this.add(urlTextField, BorderLayout.BEFORE_FIRST_LINE);
         response = new JLabel();
         response.setMinimumSize(new Dimension(150,20));
-        this.add(response, BorderLayout.CENTER);
+        this.add(response, BorderLayout.EAST);
+
+
+
+        this.add(new JLabel("Connected to the internet: " + wifiTest()), BorderLayout.SOUTH);
 
     }
 
     private void onTextInput(){
+//        String text = "";
+//        text += urlTextField.getText();
+//        URLConnection url = createUrlConnection(urlTextField.getText());
+//        if (url != null) {
+//            text += " url correct";
+//        }else{
+//            text += " url incorrect";
+//        }
         String text = "";
-        text += urlTextField.getText();
-        URLConnection url = createUrlConnection(urlTextField.getText());
-
-
-        if (url != null) {
-            text += " url correct";
+        // "https://p60-calendars.icloud.com/published/2/KQpieZ-gmqxhEDixV83NnwiNFshbA0Kv7YrWnWT6RtG4vEt0ZavpPs_Yx2xnoQA8BOLACjFBfqzO4TiaLQUM1BADNcvK_J3GwAHbUY8btOA"
+        ArrayList<String> a = getFile(urlTextField.getText());
+        text += a.size();
+        if (a.size() != 0){
+            createTable(a);
         }else{
-            text += " url incorrect";
+            text += "invalid link for ical";
         }
+        System.out.println(text);
         response.setText(text);
     }
 
@@ -116,18 +180,18 @@ public class EventFrame extends JFrame {
         boolean encodingUsed = true;
         boolean streamCreated = true;
         ArrayList<String> data = new ArrayList<String>();
+        if (!urlString.isEmpty()){
+            urlString = getUrlString(urlString);
+            // create a URL
+            try{
+                url = new URL(getUrlString(urlString));
+            }catch (MalformedURLException e){
 
-        urlString = getUrlString(urlString);
-        // create a URL
-        try{
-            url = new URL(getUrlString(urlString));
-        }catch (MalformedURLException e){
-
+            }
         }
-//        System.out.println(urlString);
+
         if (url != null){
             InputStream in = null;
-//            System.out.println("opening connection");
             try{
                 in = url.openStream();
             }catch (IOException e){
@@ -188,7 +252,8 @@ public class EventFrame extends JFrame {
 
 
 
-    private void createCalender(ArrayList<String> cal){
+    private ArrayList<IcalEvent> createCalender(ArrayList<String> cal){
+        long time = System.currentTimeMillis();
         boolean parserError = false;
         int index = 0;
         boolean event = false;
@@ -227,18 +292,16 @@ public class EventFrame extends JFrame {
                                         parserError = true;
                                         System.out.println("SUMMARY args");
                                     }
-                                } else if (cal.get(index).contains("DSTART;")) {
-                                    eventsDstart = cal.get(index).split("DSTART;");
+                                } else if (cal.get(index).contains("DTSTART;")) {
+                                    eventsDstart = cal.get(index).split("DTSTART;");
+
                                     if (eventsDstart.length == 2){
-                                        System.out.println(eventsDstart[0]);
-                                        System.out.println(eventsDstart[1]);
-                                        parserError
                                         eventsDstart = eventsDstart[1].split(":");
                                         if (eventsDstart.length == 2) {
                                             icalEvents.get(icalIndex).setTimezone(eventsDstart[0]);
                                             icalEvents.get(icalIndex).setStart(eventsDstart[1]);
                                         }else{
-                                            System.out.println("DSTART args");
+                                            System.out.println("DTSTART args");
                                             parserError = true;
                                         }
                                     }else{
@@ -248,12 +311,9 @@ public class EventFrame extends JFrame {
                                 }
                                 index ++;
                             }
-
                         } else{
                             index ++;
                         }
-
-
                     }
                 }else {
                     System.out.println("could not find version or invalid version");
@@ -264,13 +324,14 @@ public class EventFrame extends JFrame {
         }else{
             System.out.println("couldn't find start");
         }
+        System.out.println("took " + (System.currentTimeMillis()-time) + " ms");
         System.out.println("error happened? " + parserError);
         System.out.println("event state: " + event);
         System.out.println("events: " + icalEvents.size());
         System.out.println("index: " + index);
         System.out.println("version: " + version);
         icalEvents.stream().forEach(System.out::println);
-
+        return icalEvents;
     }
 
 
@@ -285,6 +346,7 @@ public class EventFrame extends JFrame {
 
         }
 
+
         public String getSummary() {
             return summary;
         }
@@ -298,11 +360,10 @@ public class EventFrame extends JFrame {
         }
 
         public void setStart(String start) {
-            System.out.println(start);
-            String[] a = start.split(":");
-            if (a.length == 2) {
-                this.date = a[0];
-                this.start = Long.parseLong(a[1]);
+            String[] split = start.split("T");
+            if (split.length == 2) {
+                this.date = split[0];
+                this.start = Long.parseLong(split[1]);
             }
 
         }
@@ -317,10 +378,13 @@ public class EventFrame extends JFrame {
 
         @Override
         public String toString() {
-            return summary + " | " + timezone + " | " + start;
+            return summary + " | " + timezone + " | " + date + " | " + start;
+        }
+
+        public Object[] toObjectArray(){
+            return new String[]{summary, timezone, date,  Long.toString(start)};
         }
     }
-
 
 
 }
